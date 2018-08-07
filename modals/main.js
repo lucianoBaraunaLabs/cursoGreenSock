39b397eb8c5d309df0497b48488d6b30b56d6cd1
Luciano Baraúna <baraunaluciano@gmail.com>
2018-08-07 14:25:58 -0300

    criando modal com animações

---
+++ modals/main.js
@@ -0,0 +1,49 @@
+// Modal Elements
+const modal = document.querySelector('.modal');
+const modalBox = document.querySelector('.modal__box');
+const modalHeader = document.querySelector('.modal__header');
+const modalTitle = document.querySelector('.modal__title');
+const modalClose = document.querySelector('.modal__close');
+
+// Buttons
+const btnClose = document.querySelector('.modal__close');
+const btnFadeIn = document.getElementById('fade-in');
+const btnFadeInScale = document.getElementById('fade-in-scale');
+const btnSlideIn = document.getElementById('slide-in');
+const btnFullScreen = document.getElementById('fullscreen');
+
+// Events
+btnFadeIn.addEventListener('click', fadeIn);
+btnClose.addEventListener('click', close);
+btnFadeInScale.addEventListener('click', fadeInScale);
+btnSlideIn.addEventListener('click', slideIn);
+btnFullScreen.addEventListener('click', fullScreen);
+
+// Animations Functions
+function fadeIn() {
+  TweenMax.to(modal, .2, { autoAlpha: 1 });
+}
+
+function fadeInScale() {
+  TweenMax.set(modalBox, { scale: 0 });
+  TweenMax.to(modal, .5, { autoAlpha: 1 });
+  TweenMax.to(modalBox, 1, { scale: 1, delay: .5, ease: Elastic.easeOut })
+}
+
+function slideIn() {
+  TweenMax.to(modal, .5, { autoAlpha: 1 });
+  TweenMax.from(modalBox, .5, { autoAlpha: 0, top: -300 })
+}
+
+function fullScreen() {
+  TweenMax.set(modal, { backgroundColor: '#fff', scale: 0 });
+  TweenMax.set(modalClose, { color: '#333' });
+  TweenMax.set([modalHeader, modalBox], { position: 'initial' });
+  TweenMax.set(modalTitle, { color: '#333', backgroundColor: '#fff' });
+  TweenMax.to(modal, .2, { autoAlpha: 1, scale: 1 });
+}
+
+function close() {
+  TweenMax.set([modal, modalClose, modalBox, modalHeader, modalTitle], { clearProps: 'all' }) // limpa todas as propriedades que estão sendo utilizadas no momento
+  TweenMax.to(modal, .2, { autoAlpha: 0 })
+}
